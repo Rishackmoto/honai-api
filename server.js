a46dd36416415c9df86aa59f82947800567a3b95
Rishackmoto <rishackmoto@gmail.com>
2026-06-24 14:48:11 +0900

    clean server for login only

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
 }));
+app.options('*', cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(express.json());
 
