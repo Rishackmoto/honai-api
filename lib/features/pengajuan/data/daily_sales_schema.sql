86e8f5299af39aba3be2b7d8a65941911b4605ae
Rishackmoto <rishackmoto@gmail.com>
2026-09-15 23:07:44 +0900

    clean server for login only

--- lib/features/pengajuan/data/daily_sales_schema.sql
+++ lib/features/pengajuan/data/daily_sales_schema.sql
@@ -16,3 +16,18 @@ BEGIN
   );
   CREATE INDEX IX_daily_sales_user_date ON dbo.daily_sales_activity(userid, tanggal);
 END;
+IF OBJECT_ID('dbo.daily_sales_attachment', 'U') IS NULL
+BEGIN
+  CREATE TABLE dbo.daily_sales_attachment (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    activity_id INT NOT NULL REFERENCES dbo.daily_sales_activity(id),
+    filename NVARCHAR(180) NOT NULL,
+    content_type VARCHAR(50) NOT NULL,
+    file_size INT NOT NULL CHECK (file_size > 0 AND file_size <= 5242880),
+    file_data VARBINARY(MAX) NOT NULL,
+    created_at DATETIME2 NOT NULL DEFAULT SYSDATETIME()
+  );
+  CREATE INDEX IX_daily_sales_attachment_activity ON dbo.daily_sales_attachment(activity_id);
+END;
+IF OBJECT_ID('dbo.daily_sales_migration', 'U') IS NULL
+  CREATE TABLE dbo.daily_sales_migration (version INT PRIMARY KEY);
